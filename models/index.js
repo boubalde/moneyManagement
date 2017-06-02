54b5fd34b717cf6eba09e998dc26dc52a6f16e84
Esther Maxi <esmaxi21@gmail.com>
2017-06-01 20:44:14 -0400

    changed a whole lot

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@
 
 var fs        = require('fs');
 var path      = require('path');
-const Sequelize = require('sequelize');
+var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
@@ -38,7 +38,5 @@ Object.keys(db).forEach(function(modelName) {
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
-
 
 module.exports = db;
