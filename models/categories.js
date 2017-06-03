362e836cb194e8bf112fb02ae7b95235a197e5d3
rutweb5d <clauderobo@outlook.com>
2017-06-02 23:59:30 -0400

    Commit 06/02/17

--- models/categories.js
+++ models/categories.js
@@ -10,9 +10,6 @@ module.exports = function(sequelize, DataTypes) {
       primaryKey: true,
       allowNull: false
     },
-    category_name: {
-      type:DataTypes.STRING,
-    }
     description: {
       type: DataTypes.TEXT,
     }
